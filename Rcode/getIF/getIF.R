931927a9d42cd50fe914fe79b85a3bb4fab51a3c
chaimol <chaimol@163.com>
2021-06-16 15:28:33 +0800

    add a readme for getIF

--- Rcode/getIF/getIF.R
+++ Rcode/getIF/getIF.R
@@ -1,25 +1,25 @@
-install.packages("scholar")
-install.packages("dplyr")
+if(!require(dplyr))install.packages("scholar")
+if(!require(dplyr))install.packages("dplyr")
 library(scholar)
 library(dplyr)
-setwd("e:/bioinformation_center/Rcode/getIF/")
+
 #scholar这个包实质是爬取了谷歌学术，整理对应的内容。简单粗暴好用。
 
 #设定期刊的名称的向量
-jn = c("bioinformatics","methods is ecology and evolution","molecular biosystems","molecular biology and evolution")
+# jn = c("bioinformatics","methods is ecology and evolution","molecular biosystems","molecular biology and evolution")
+# 
+# #获取对应期刊的影响因子
+# get_impactfactor(jn)
+# 
+# #获取cns的最新影响因子
+# cns=c("cell","nature","science")
+# get_impactfactor(cns)
+# 
+# #获取有bioinformatics相关的所有期刊
+# bioinfor <- agrep("bioinformatics",scholar:::impactfactor$Journal,ignore.case = T,value = T,max.distance = 0.05)
+# get_impactfactor(bioinfor)
 
-#获取对应期刊的影响因子
-get_impactfactor(jn)
 
-#获取cns的最新影响因子
-cns=c("cell","nature","science")
-get_impactfactor(cns)
-
-#获取有bioinformatics相关的所有期刊
-bioinfor <- agrep("bioinformatics",scholar:::impactfactor$Journal,ignore.case = T,value = T,max.distance = 0.05)
-get_impactfactor(bioinfor)
-
-
-journal_name <- read.csv("Journal.csv",header = TRUE)$journal
+journal_name <- read.csv("JournalName.csv",header = TRUE)$Journal
 output <- get_impactfactor(journal_name)
 write.csv(output,"journal_IF.csv")
---
+++ Rcode/getIF/readme.md
@@ -0,0 +1,10 @@
+## purpose
+for get IF in biology science.
+## Usage
+`Rscript getIF.R`
+you also can add in line in file `JournalName.csv` the first  column.
+## output
+`getIF.csv` is the output IF file.
+
+
+
